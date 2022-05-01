51dfa41febad8a41cef198992f4e8a1942245a76
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-01 17:15:05 -0500

    planning for js

---
+++ assets/js/script1.js
@@ -0,0 +1,4 @@
+//1. Connecting the Dom
+//2. Define variable for interactivity. 
+//3. Add onclick event listeners to buttons.
+//4. Write functions: start, prev, next, submit, generate player, show leaderPage, show endPage, restart the game. 
